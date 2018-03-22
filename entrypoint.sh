47722cc0d9b96f8b57a03a789e43f9375798dc80
RekGRpth <rekgrpth@gmail.com>
2018-03-22 11:22:18 +0500

    up

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,15 +16,17 @@ PGADMIN="/usr/lib/python3.6/site-packages/pgadmin4"
 
 sed -i "s|'/var/lib/pgadmin'|'$HOME'|gi" "$PGADMIN/config.py"
 sed -i "s|'/var/log/pgadmin/pgadmin4.log'|'$HOME/log/pgadmin4.log'|gi" "$PGADMIN/config.py"
-sed -i "/^UPGRADE_CHECK_ENABLED/cUPGRADE_CHECK_ENABLED = False" "$PGADMIN/config.py"
+#sed -i "/^UPGRADE_CHECK_ENABLED/cUPGRADE_CHECK_ENABLED = False" "$PGADMIN/config.py"
 sed -i "/^DEFAULT_SERVER/cDEFAULT_SERVER = '0.0.0.0'" "$PGADMIN/config.py"
 
 test -f "$PGADMIN/pgAdmin4.wsgi" && mv -f "$PGADMIN/pgAdmin4.wsgi" "$PGADMIN/pgAdmin4wsgi.py"
 cp -rf $PGADMIN/* "$HOME/app"
 
-export PGADMIN_SETUP_EMAIL=container@pgadmin.org
-export PGADMIN_SETUP_PASSWORD=Conta1ner
-python3 "$HOME/app/setup.py"
+if [ ! -f "$HOME/pgadmin4.db" ]; then
+    export PGADMIN_SETUP_EMAIL=container@pgadmin.org
+    export PGADMIN_SETUP_PASSWORD=Conta1ner
+    python3 "$HOME/app/setup.py"
+fi
 
 find "$HOME" ! -group "$GROUP" -exec chgrp "$GROUP_ID" {} \;
 find "$HOME" ! -user "$USER" -exec chown "$USER_ID" {} \;
