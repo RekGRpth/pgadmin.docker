aa10a486cafa8d1251e1644557ff5d8b4b4cccc0
RekGRpth <rekgrpth@gmail.com>
2018-09-25 21:31:07 +0500

    up

--- run.sh
+++ run.sh
@@ -6,14 +6,15 @@ docker stop pgadmin
 docker rm pgadmin
 docker pull rekgrpth/pgadmin || exit $?
 docker volume create pgadmin || exit $?
+docker network create my
 docker run \
     --add-host `hostname -f`:`ip -4 addr show docker0 | grep -oP 'inet \K[\d.]+'` \
     --detach \
     --env GROUP_ID=$(id -g) \
     --env USER_ID=$(id -u) \
     --hostname pgadmin \
-    --link postgres \
     --name pgadmin \
+    --network my \
     --restart always \
     --volume pgadmin:/data \
     rekgrpth/pgadmin
