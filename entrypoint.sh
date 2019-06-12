ec436d3873c8fdd9d3fb3a49b978d6224cba9641
RekGRpth <rekgrpth@gmail.com>
2019-06-12 08:46:56 +0500

    up

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,7 +16,7 @@ if [ ! -f "$HOME/config/pgadmin4.db" ]; then
     python "/usr/local/lib/python3.7/site-packages/pgadmin4/setup.py"
 fi
 
-find "$HOME" ! -group "$GROUP" -exec chgrp "$GROUP_ID" {} \;
-find "$HOME" ! -user "$USER" -exec chown "$USER_ID" {} \;
+#find "$HOME" ! -group "$GROUP" -exec chgrp "$GROUP_ID" {} \;
+#find "$HOME" ! -user "$USER" -exec chown "$USER_ID" {} \;
 
 exec su-exec "$USER" "$@"
