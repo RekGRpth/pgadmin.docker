06ac50b635023c2c9fad9f4258f71f9015ef67de
RekGRpth <rekgrpth@gmail.com>
2020-03-11 10:47:18 +0500

    up

--- service.sh
+++ service.sh
@@ -4,19 +4,15 @@
 #docker push rekgrpth/pgadmin || exit $?
 docker pull rekgrpth/pgadmin || exit $?
 docker volume create pgadmin || exit $?
-docker network create --attachable --opt com.docker.network.bridge.name=docker docker || echo $?
-docker stop pgadmin || echo $?
-docker rm pgadmin || echo $?
-docker run \
-    --detach \
+docker network create --attachable --driver overlay docker || echo $?
+docker service create \
     --env GROUP_ID=$(id -g) \
     --env LANG=ru_RU.UTF-8 \
     --env TZ=Asia/Yekaterinburg \
     --env USER_ID=$(id -u) \
     --hostname pgadmin \
+    --mount type=bind,source=/etc/certs,destination=/etc/certs \
+    --mount type=volume,source=pgadmin,destination=/home \
     --name pgadmin \
     --network name=docker \
-    --restart always \
-    --volume /etc/certs:/etc/certs \
-    --volume pgadmin:/home \
     rekgrpth/pgadmin uwsgi --ini pgadmin.ini
