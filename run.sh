5358cf2190a3216f1169cff3053265b63eef1bc3
RekGRpth <rekgrpth@gmail.com>
2020-04-09 13:49:31 +0500

    up

--- run.sh
+++ run.sh
@@ -14,11 +14,11 @@ docker run \
     --env TZ=Asia/Yekaterinburg \
     --env USER_ID=$(id -u) \
     --hostname pgadmin \
+    --mount type=bind,source=/etc/certs,destination=/etc/certs,readonly \
+    --mount type=bind,source=/run/postgresql,destination=/run/postgresql \
+    --mount type=bind,source=/run/uwsgi,destination=/run/uwsgi \
+    --mount type=volume,source=pgadmin,destination=/home \
     --name pgadmin \
     --network name=docker \
     --restart always \
-    --volume /etc/certs:/etc/certs \
-    --volume pgadmin:/home \
-    --volume /run/postgresql:/run/postgresql \
-    --volume /run/uwsgi:/run/uwsgi \
     rekgrpth/pgadmin uwsgi --ini pgadmin.ini
