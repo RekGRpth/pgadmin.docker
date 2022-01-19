06e5f04c0a283bd0f87940e077d6eec4477dc7a1
RekGRpth <rekgrpth@gmail.com>
2022-01-19 16:56:07 +0500

    fix

--- bin/docker_clean_alpine.sh
+++ bin/docker_clean_alpine.sh
@@ -38,7 +38,7 @@ apk add --no-cache --virtual .pgadmin-rundeps \
     py3-urllib3 \
     py3-wtforms \
     uwsgi-python3 \
-    $(scanelf --needed --nobanner --format '%n#p' --recursive /usr/local | tr ',' '\n' | sort -u | while read -r lib; do if [ ! "$(find /usr/local/lib -name "$lib")" ]; then echo "so:$lib"; fi; done) \
+    $(scanelf --needed --nobanner --format '%n#p' --recursive /usr/local | tr ',' '\n' | sort -u | while read -r lib; do test -z "$(find /usr/local/lib -name "$lib")" && echo "so:$lib"; done) \
 ;
 find /usr/local/bin -type f -exec strip '{}' \;
 find /usr/local/lib -type f -name "*.so" -exec strip '{}' \;
